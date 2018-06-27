301409f12c743d48fdffc7b242530bcd14463903
lalit-nagarro <lalit.haryani@nagarro.com>
2018-06-27 14:31:10 +0530

    commit

--- user.rb
+++ user.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 class User
 
-	attr_reader :name
+	PER_PAGE = 5
 
 	def initialize
 	end
@@ -11,9 +11,11 @@ class User
 		def search(params)
 			users = {}
 			q = params["name"]
+			offset, limit = get_offset_and_limit(params["page"])
 			if q && q != ""
 				users_data = parse_file
-				users = users_data.select{|key, value| value["name"] == q}.values
+				users = users_data.select{|key, value| value["name"] == q}.values[offset, limit]
+				users = users.first(PER_PAGE) if users
 			end
 			
 			#return users json
@@ -24,6 +26,16 @@ class User
 		def parse_file
 			YAML::load_file(File.join(File.dirname(File.expand_path(__FILE__)), 'user_fixture.yml'))
 		end
+
+
+		def get_offset_and_limit(page)
+		      page = page ? page.to_i : 1
+		      offset = (page - 1) * PER_PAGE + (page == 1 ? 0 : 1)
+		      limit =  (offset - 1) + PER_PAGE + 1
+
+		      return offset, limit
+		end
+
 	end
 
 
